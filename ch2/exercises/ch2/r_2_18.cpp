e2f9ab06a960d89aeac620dfa0a44b3cfdd2ac3a
Shawn Ogbomo <Shawn_Ogbomo@hotmail.com>
2025-01-20 04:49:12 -0500

    -update

---
+++ ch2/exercises/ch2/r_2_18.cpp
@@ -0,0 +1,47 @@
+//Write a short C++ program that creates a Pair class that can store two
+//objects declared as generic types.Demonstrate this program by creating
+//and printing Pair objects that contain five different kinds of pairs,such as
+//<int, string> and <float, long>
+#include <iostream>
+using namespace std::literals;
+
+template <typename T1, typename T2>
+class Pair
+{
+public:
+	Pair(const T1& t1, const T2& t2)
+		:v1{ t1 },
+		v2{ t2 }
+	{
+	}
+
+	static friend auto operator << (std::ostream& os, const Pair<T1, T2>& p) -> std::ostream&
+	{
+		return os << p.v1 << " " << p.v2 << "\n";
+	}
+
+private:
+	T1 v1{};
+	T2 v2{};
+};
+
+auto main() -> int
+{
+	std::cout << "Enter pairs...\nPress ctrl+z to exit...\n\n";
+
+	try
+	{
+		auto p1 = Pair{ 7,"Shawn"sv };
+		auto p2 = Pair{ 'S',77.7 };
+		auto p3 = Pair{ "Shawn7"s,'*' };
+		auto p4 = Pair{ true,"True"sv };
+		auto p5 = Pair{ static_cast<float>(7.7), static_cast<long>(9) };
+
+		std::cout << p1 << p2 << p3 << p4 << p5;
+	}
+	catch (const std::ios_base::failure& fail)
+	{
+		std::cerr << fail.what() << "\n";
+		return 1;
+	}
+}
